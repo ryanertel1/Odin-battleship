be30becce04f60167e7f12e70e0c9e1ec58f4e5e
Ryan Ertel <122134901+ryanertel1@users.noreply.github.com>
2025-03-30 22:21:37 -0400

    Initial commit

---
+++ ship.js
@@ -0,0 +1,22 @@
+export default class Ship {
+    constructor(length) {
+        this.hits = null;
+        this.length = length || null;
+        this.sunk = false;
+    }
+
+    hit() {
+        if(this.sunk === false) {
+            this.hits += 1;
+            this.isSunk();
+            return true;
+        }
+    }
+
+    isSunk() {
+        if(this.hits >= this.length) {
+            this.sunk = true;
+            return true;
+        } else { return false; }
+    }
+}
---
+++ ship.test.js
@@ -0,0 +1,24 @@
+import Ship from './ship.js'
+
+let battleship = new Ship(4);
+
+test('constructor creates ship object correctly', () => {
+    expect(new Ship()).toBeInstanceOf(Ship);
+});
+
+test('ship length is registered correctly', () => {
+    expect(new Ship(3).length).toBe(3);
+});
+
+test('ship hit method registers correctly', () => {
+    let battleship = new Ship(4);
+    battleship.hit();
+    expect(battleship.hits).toBe(1);
+});
+
+test('ship isSunk method registers when ship is sunk', () => {
+    let patrol = new Ship(2);
+    patrol.hit();
+    patrol.hit();
+    expect(patrol.sunk).toBe(true);
+});
